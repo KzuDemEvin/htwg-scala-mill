2e603d31dc5d56106784e0564bc82c8c22e3050a
Kevin Schoch <kevin.schoch@outlook.com>
2020-05-22 16:00:26 +0200

    updated StoneSpec file

--- src/main/scala/de/htwg/se/mill/model/Stone.scala
+++ src/main/scala/de/htwg/se/mill/model/Stone.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.mill.model
 
-class Stone(value: Boolean, color: String) {
+case class Stone(value: Boolean, color: String) {
   def isSet:Boolean = value
   def whichColor:String = color
 }
--- src/test/scala/de/htwg/se/mill/model/StoneSpec.scala
+++ src/test/scala/de/htwg/se/mill/model/StoneSpec.scala
@@ -1,15 +1,38 @@
 package de.htwg.se.mill.model
 
-import org.scalatest._
+import org.scalatest.{Matchers, WordSpec}
 
 class StoneSpec extends WordSpec with Matchers {
-  "A Player" when { "new" should {
-    val player = Player("Your Name")
-    "have a name"  in {
-      player.name should be("Your Name")
+  "A Stone" when {
+    "new" should {
+      val newStone = Stone(false, "white")
+      "have a value set to false" in {
+        newStone.value should be(false)
+      }
+      "have a color" in {
+        val stoneWhite = Stone(false, "white")
+        val stoneBlack = Stone(false, "black")
+        "which is white" in {
+          stoneWhite.color should be("white")
+        }
+        "which is black" in
+          stoneBlack.toString should be("Your Name")
+      }
     }
-    "have a nice String representation" in {
-      player.toString should be("Your Name")
+    "is placed" should {
+        val stone = Stone(true, "white")
+        "have a value set to true" in {
+          stone.value should be(true)
+        }
+      "have a color" in {
+        val stoneWhite = Stone(false, "white")
+        val stoneBlack = Stone(false, "black")
+        "which is white" in {
+          stoneWhite.color should be("white")
+        }
+        "which is black" in
+          stoneBlack.toString should be("Your Name")
+      }
     }
-  }}
+  }
 }
