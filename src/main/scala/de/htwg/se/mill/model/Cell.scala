dcf6392d691c7fbc3d7718c4d1a43015eeb6f60e
Kevin Schoch <kevin.schoch@outlook.com>
2020-06-05 18:23:13 +0200

    Updated Cell.scala and CellSpec.scala

--- src/main/scala/de/htwg/se/mill/model/Cell.scala
+++ src/main/scala/de/htwg/se/mill/model/Cell.scala
@@ -1,10 +1,9 @@
 package de.htwg.se.mill.model
 
 case class Cell(filled:Boolean, content: Stone) {
-  def this(filled:Boolean = false) {
-    this(filled, Stone(0, Color.noColor))
+  def this() {
+    this(false, Stone(0, Color.noColor))
   }
-
   def isSet: Boolean = filled
   def getContent:Stone = content
 }
