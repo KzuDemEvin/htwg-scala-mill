6a384c58c701f4a735f62c8fdaf85e434c5cab8d
Kevin Schoch <kevin.schoch@outlook.com>
2020-06-08 15:49:06 +0200

    Updated for Travis CI implementation

--- src/main/scala/de/htwg/se/mill/model/Player.scala
+++ src/main/scala/de/htwg/se/mill/model/Player.scala
@@ -4,6 +4,7 @@ case class Player(name: String, amountStones: Int = 9) {
    def this (name: String) {
       this(name, 9)
    }
+
    override def toString:String = "Name: " + name + ", Amount of Stones: " + amountStones
 }
 
