5877b543e72c8531eafe9160536b05664cc62bfa
Manuel Günter <guenter.manuel@web.de>
2020-06-19 10:15:27 +0200

    renamed

--- src/main/scala/de/htwg/se/mill/model/StrategyPattern.scala
+++ src/main/scala/de/htwg/se/mill/model/StrategyPattern.scala
@@ -1,15 +1,10 @@
 package de.htwg.se.mill.model
 
-trait FieldCreateStrategyTemplate {
+trait StrategyPattern {
 
   def createNewField(size:Int): Field = {
     var field  = new Field(size)
-    field = prepare(field)
     field = fill(field)
-    field
-  }
-
-  def prepare(field: Field): Field = {
     field
   }
 
--- src/test/scala/de/htwg/se/mill/model/FieldCreateRandomStrategySpec.scala
+++ src/test/scala/de/htwg/se/mill/model/FieldCreateRandomStrategySpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{Matchers, WordSpec}
 class FieldCreateRandomStrategySpec extends WordSpec with Matchers {
   "A FieldCreator " should {
     "create an empty Field and fill it with stones with a creation strategy" in {
-      val field= (new FieldCreateRandomStrategy).createNewField(7)
+      val field= (new RandomStrategy).createNewField(7)
       field.placedStones() should be(24)
       field.available(0,0) should be(false)
     }
