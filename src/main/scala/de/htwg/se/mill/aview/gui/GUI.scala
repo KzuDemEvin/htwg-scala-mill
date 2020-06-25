d1a0ddf6579cab92d17c77464a5323f1ba078aa9
Manuel Günter <guenter.manuel@web.de>
2020-06-25 15:48:18 +0200

    testing

--- src/main/scala/de/htwg/se/mill/aview/gui/GUI.scala
+++ src/main/scala/de/htwg/se/mill/aview/gui/GUI.scala
@@ -1,18 +1,41 @@
 package de.htwg.se.mill.aview.gui
 
-import de.htwg.se.mill.controller.Controller
+import de.htwg.se.mill.controller.{CandidatesChanged, CellChanged, Controller}
 
 import scala.swing.event.Key
-import scala.swing.{Action, BoxPanel, GridPanel, Label, MainFrame, Menu, MenuBar, MenuItem, Orientation}
+import scala.swing.{Action, BorderPanel, BoxPanel, GridPanel, Label, MainFrame, Menu, MenuBar, MenuItem, Orientation, TextField}
 
 
 class GUI(controller: Controller) extends MainFrame {
-  title = "HTWG MILL"
-  visible = true
+  listenTo(controller)
 
-  contents = new BoxPanel(Orientation.Vertical) {
-    contents += new GUIGridPanel(controller).gridPanel
-    contents += new GUIMenuBar(controller).menuBar
+  title = "Mill"
+  var cells = Array.ofDim[CellPanel](controller.fieldsize, controller.fieldsize)
+
+
+  menuBar = new GUIMenuBar(controller).menuBar
+
+
+  val gridPanel = new GUIGridPanel(controller, cells).gridPanel
+  val statusline = new TextField(controller.statusText, 20)
+
+
+  contents = new BorderPanel {
+    add(gridPanel, BorderPanel.Position.Center)
+    add(statusline, BorderPanel.Position.South)
+  }
+
+  visible = true
+  updateField
+
+  reactions += {
+    case event: CellChanged     => updateField
+    case event: CandidatesChanged => updateField
+  }
+
+  def updateField: Unit = {
+    statusline.text = controller.statusText
+    repaint
   }
 
 }
