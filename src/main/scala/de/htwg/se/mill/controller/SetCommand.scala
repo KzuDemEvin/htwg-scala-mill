d0b23e656fc7ebaee15e84e1de4e269e57ea393f
Manuel Günter <guenter.manuel@web.de>
2020-06-26 11:38:49 +0200

    update

--- src/main/scala/de/htwg/se/mill/controller/SetCommand.scala
+++ src/main/scala/de/htwg/se/mill/controller/SetCommand.scala
@@ -4,7 +4,7 @@ import de.htwg.se.mill.model.{Cell, Stone}
 import de.htwg.se.mill.util.Command
 
 class SetCommand(row:Int, col: Int, value:Cell, controller: Controller) extends Command {
-  override def doStep: Unit = controller.field = controller.field.set(row, col, value)
+  override def doStep: Unit = {controller.field = controller.field.set(row, col, value)}
 
   override def undoStep: Unit = controller.field = controller.field.set(row, col, Cell(false, Stone("n")))
 
