04d82e6db4a4fb26ba52bb907f3cbb6a2407f5f8
Kevin Schoch <kevin.schoch@outlook.com>
2020-06-26 11:37:58 +0200

    Fixed Specs

--- src/test/scala/de/htwg/se/mill/controller/GameStateSpec.scala
+++ src/test/scala/de/htwg/se/mill/controller/GameStateSpec.scala
@@ -7,7 +7,7 @@ class GameStateSpec extends WordSpec with Matchers {
     "should represent the game current sate. It" should {
       "be in progress" in {
         val gameStateProgress = GameState.handle(NewState())
-        gameStateProgress should be("Game in progress")
+        gameStateProgress should be("New field")
       }
       "be finished" in {
         val gameStateFinished = GameState.handle(FinishedState())
@@ -20,6 +20,14 @@ class GameStateSpec extends WordSpec with Matchers {
       "say its black's turn" in {
         val gameStateBlack = GameState.handle(BlackTurnState())
         gameStateBlack should be("Black's turn")
+      }
+      "say its a whitemill" in {
+        val gameStateWhiteMill = GameState.handle(WhiteMillState())
+        gameStateWhiteMill should be("White Mill")
+      }
+      "say its a blackmill" in {
+        val gameStateBlackMill = GameState.handle(BlackMillState())
+        gameStateBlackMill should be("Black Mill")
       }
     }
 
