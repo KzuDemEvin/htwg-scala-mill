7a5ee2c8c1959a2e52ba8aca9fe0c69c35c9b1e0
Manuel Günter <guenter.manuel@web.de>
2020-07-07 22:00:49 +0200

    fix bug with move/fly if/else

--- src/main/scala/de/htwg/se/mill/controller/controllerComponent/ControllerInterface.scala
+++ src/main/scala/de/htwg/se/mill/controller/controllerComponent/ControllerInterface.scala
@@ -17,7 +17,7 @@ trait ControllerInterface extends Publisher {
   def selectDriveCommand():ModeState
   def handleClick(row: Int, column: Int): Unit
   def handleSet(row:Int, column:Int, counter:Int):Int
-  def handleMoveAndFly(row:Int, column:Int, counter:Int):Int
+  def handleMoveAndFly(row:Int, column:Int, counter:Int, move:ModeState):Int
   def set(row: Int, col: Int): Unit
   def moveStone(rowOld: Int, colOld: Int, rowNew: Int, colNew: Int): Unit
   def fly(rowOld: Int, colOld: Int, rowNew: Int, colNew: Int):Unit
