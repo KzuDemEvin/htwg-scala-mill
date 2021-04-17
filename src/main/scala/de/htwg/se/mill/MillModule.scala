4ec90c42b06c86a946d585cd366b24a0cb4c838e
Kevin Schoch <kevin.schoch@outlook.com>
2021-04-17 19:13:45 +0200

    Remove redundant code

--- src/main/scala/de/htwg/se/mill/MillModule.scala
+++ src/main/scala/de/htwg/se/mill/MillModule.scala
@@ -8,8 +8,6 @@ import de.htwg.se.mill.controller.controllerComponent._
 import de.htwg.se.mill.model.fieldComponent.fieldBaseImpl.RandomStrategy
 import de.htwg.se.mill.model.fieldComponent.FieldInterface
 import de.htwg.se.mill.model.fieldComponent.fieldAdvancedImpl.Field
-import de.htwg.se.mill.model.fileIoComponent._
-import de.htwg.se.mill.model.fileIoComponent.fileToXmlImpl.FileIO
 
 
 class MillModule extends AbstractModule with ScalaModule {
@@ -24,9 +22,6 @@ class MillModule extends AbstractModule with ScalaModule {
 
     bind[FieldInterface].annotatedWithName("normal").toInstance(new Field(defaultSize))
     bind[FieldInterface].annotatedWithName("random").toInstance((new RandomStrategy).createNewField(defaultSize))
-
-    bind[FileIOInterface].to[FileIO] //XML
-    //bind[FileIOInterface].to[fileIoJsonImpl.FileIO] //JSON
   }
 
 }
