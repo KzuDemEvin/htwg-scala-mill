a5f9ec7ecf6a5cee85941d3dfa7bbcbd0ef4fcd5
Kevin Schoch <kevin.schoch@outlook.com>
2021-04-21 09:16:35 +0200

    Ready for docker compose

--- src/main/scala/de/htwg/se/mill/aview/HttpServer.scala
+++ src/main/scala/de/htwg/se/mill/aview/HttpServer.scala
@@ -17,15 +17,16 @@ case class HttpServer(controller: ControllerInterface) {
 
   val interface: String = "0.0.0.0"
   val port: Int = 8080
+  val uriPath: String = "mill"
 
   val route: Route =
     concat(
-    path("mill") {
+    path(uriPath) {
       get {
         fieldToHtml
       }
     } ~
-      path("mill" / "player") {
+      path(uriPath / "player") {
         get {
           parameters("name", "number") {
             (name, number) =>
@@ -34,43 +35,43 @@ case class HttpServer(controller: ControllerInterface) {
           }
         }
       } ~
-      path("mill" / "new") {
+      path(uriPath / "new") {
         get {
           controller.createEmptyField(size)
           fieldToHtml
         }
       } ~
-      path("mill" / "random") {
+      path(uriPath / "random") {
         get {
           controller.createRandomField(size)
           fieldToHtml
         }
       } ~
-      path("mill" / "save") {
+      path(uriPath / "save") {
         get {
           controller.save()
           fieldToHtml
         }
       } ~
-      path("mill" / "load") {
+      path(uriPath / "load") {
         get {
           controller.load()
           fieldToHtml
         }
       } ~
-      path("mill" / "undo") {
+      path(uriPath / "undo") {
         get {
           controller.undo
           fieldToHtml
         }
       } ~
-      path("mill" / "redo") {
+      path(uriPath / "redo") {
         get {
           controller.redo
           fieldToHtml
         }
       } ~
-      path("mill" / Segment) { command => {
+      path(uriPath / Segment) { command => {
         get {
           processInputLine(command)
           fieldToHtml
@@ -150,6 +151,8 @@ case class HttpServer(controller: ControllerInterface) {
   }
 
   val bindingFuture: Future[Http.ServerBinding] = Http().newServerAt(interface, port).bind(route)
+
+  println(s"Mill server is online at http://${interface}:${port}/${uriPath}")
 
   def unbind(): Unit = {
     bindingFuture
